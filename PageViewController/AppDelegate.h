422e16992b1230e6947a9070698ebd20aa52edeb
duks <duks@robooot.com>
2017-01-10 15:00:54 +0800

    Initial Commit

---
+++ PageViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewController
+//
+//  Created by aDu on 2017/1/10.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
